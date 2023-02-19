5c166394af8d0363961607dc6abe434ab17795ae
MiltonAF <mafelizzolam@gmail.com>
2023-02-19 17:20:25 -0500

    subiendo numero if

--- EjerciciosTema4/src/Main.java
+++ EjerciciosTema4/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        //Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+        Scanner num = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int numeroIf = num.nextInt();
+        if(numeroIf > 0){
+            System.out.println("es positivo");
+        }else if(numeroIf < 0){
+            System.out.println("es negativo");
+        }else{
+            System.out.println("es cero");
+        }
     }
 }
