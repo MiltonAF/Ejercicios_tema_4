c99a040217537da63b2384b6a5c0b1b4940368ac
MiltonAF <mafelizzolam@gmail.com>
2023-02-19 17:31:58 -0500

    ejercicio while

--- EjerciciosTema4/src/Main.java
+++ EjerciciosTema4/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("|| EJERCICIO DEL IF ||");
 
         //Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
         Scanner num = new Scanner(System.in);
@@ -14,6 +15,23 @@ public class Main {
             System.out.println("es negativo");
         }else{
             System.out.println("es cero");
+        }
+
+
+        System.out.println("|| EJERCICIO DEL WHILE ||");
+
+        //Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+        //Incrementar el valor de la variable en uno cada vez que se ejecute.
+        //Mostrarlo por pantalla cada vez que se ejecute.
+
+        Scanner nume = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+
+        int numeroWhile = nume.nextInt();
+
+        while (numeroWhile < 3){
+            System.out.println("El numero: " + numeroWhile);
+            numeroWhile++;
         }
     }
 }
