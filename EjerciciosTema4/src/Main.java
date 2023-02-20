4ef96a8518e97b9aaf2ab4fa678316c44b1ac222
MiltonAF <mafelizzolam@gmail.com>
2023-02-19 19:00:23 -0500

    subiendo

--- EjerciciosTema4/src/Main.java
+++ EjerciciosTema4/src/Main.java
@@ -43,5 +43,37 @@ public class Main {
             System.out.println(numerodowhile);
             numerodowhile++;
         }while (numerodowhile < 3);
+
+        System.out.println("|| SENTENCIA FOR ||");
+
+        //Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que la variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.
+
+        for(int numeroFor = 0; numeroFor < 3; numeroFor++){
+            System.out.println(numeroFor);
+        }
+
+        System.out.println("|| SENTENCIA SWITCH");
+
+        //Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación.
+
+        var estacion = "Verano";
+
+        switch (estacion){
+            case "Primavera":
+                System.out.println("Es primavera");
+                break;
+            case "Verano":
+                System.out.println("Es verano");
+                break;
+            case "Otoño":
+                System.out.println("Es otoño");
+                break;
+            case "Invierno":
+                System.out.println("Es invierno");
+                break;
+            default:
+                System.out.println("Estacion no encontrada");
+
+        }
     }
 }
